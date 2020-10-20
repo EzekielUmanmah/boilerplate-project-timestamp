95c2caec716908f5d6f544c4f6438f10f603ddba
EzekielUmanmah <umanmah234@gmail.com>
2020-10-20 20:37:33 +0000

    Project finished months ago but now enabled saving on github

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,14 +18,43 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.use((req,res,next)=>{
+  console.log(req);
+  next();
+})
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//timestamp project
+app.get('/api/timestamp/:date_string?', (req,res)=>{
 
+  let date = req.params.date_string;
 
+if(date == undefined){
+    return res.json({
+      unix: new Date().getTime(),
+      utc: new Date().toUTCString()
+    })
+  }
+  else if(isNaN(date) && new Date(date) != 'Invalid Date'){
+    return res.json({
+      unix: new Date(date).getTime(),
+      utc: new Date(date).toUTCString()
+    })
+  }
+  else if(!isNaN(date)){
+    date = parseInt(date);
+    return res.json({
+      unix: new Date(date).getTime(),
+      utc: new Date(date).toUTCString()
+    })
+  }
+  return res.json({error: "Invalid Date"})
+})
+//Sun, 20 Nov 2016 17:31:29 GMT
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
